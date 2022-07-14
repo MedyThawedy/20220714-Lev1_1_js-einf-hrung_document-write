6c2ad1ccd34805ffe3bc0276a85ecf36d726ce44
MD <dhmedy@googlemail.com>
2022-07-14 14:54:30 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+let myH1 = '<h1>JavaScript ist great!!! </h1>';
+document.write(myH1);
+
+let myP = '<p>Have a nice day with JavaScript!!!<p>';
+document.write(myP);
+
+let myText = 'Hello again';
+document.write(myText);
+document.write('<br>');
+
+let vorname = 'fname';
+let nachname = 'lname';
+
+document.write('First name = ' + ' ' + vorname);
+document.write('<br>');
+document.write(' Last name = ' + ' ' + nachname);
